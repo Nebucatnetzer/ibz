70d19e4670f2ba42bc253c025aad054d2d1e0857
Andreas Zweili <andreas@capricorn>
2017-06-03 07:30:41 +0100

    solution to a)

--- db/3_sem/recursion_excercise/recursion_excercise/selections.sql
+++ db/3_sem/recursion_excercise/recursion_excercise/selections.sql
@@ -11,3 +11,19 @@ select t.TNr, t.Bezeichnung, t.Typ,
     s.Position, s.OTeil, s.Menge from teile t
     inner join struktur s
         on t.TNr = s.OTeil;
+
+select * from struktur
+where struktur.OTeil=60;
+
+select * from dbo.teile
+	where teile.TNr in (2,41,62);
+
+select	t1.TNr,
+		t1.Bezeichnung,
+		s1.UTeil,
+		t2.Bezeichnung
+	from teile t1 inner join struktur s1
+		on t1.TNr = s1.OTeil
+	inner join teile t2
+		on t2.TNr = s1.UTeil
+	where t1.TNr=60
