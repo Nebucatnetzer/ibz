299b12e46ff08d326746f4f510fd4a5214013fdb
Andreas Zweili <andreas@capricorn>
2017-06-10 08:43:15 +0200

    add a solution about iterations
    i have to break the naming conventions with the numbers because Rider
    can't create solutions with this scheme.

---
+++ software/1_sem/iterations/iterations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+namespace iterations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int input_number;
+            string result = string.Empty;
+            {
+                Console.WriteLine("Please enter a number:");
+                input_number = int.Parse(Console.ReadLine());
+                for (int i = 0; i < input_number; i++)
+                {
+                    if (i % 7 == 0)
+                    {
+                        result = result + i.ToString() + ",";
+                    }
+                }
+                
+                Console.WriteLine(result);
+                
+            }
+
+
+        }
+    }
+}
