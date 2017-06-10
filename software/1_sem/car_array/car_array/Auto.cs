de49c9fa9f223bf10dd786457c0607eece9befae
Andreas Zweili <andreas@capricorn>
2017-06-10 10:14:53 +0200

    add excercise about car arrays

---
+++ software/1_sem/car_array/car_array/Auto.cs
@@ -0,0 +1,7 @@
+namespace car_array
+{
+    public class Auto
+    {
+        public string Autonummer { get; set; }
+    }
+}
---
+++ software/1_sem/car_array/car_array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace car_array
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Auto[] arr = new Auto[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = new Auto();
+                arr[i].Autonummer = "BE12345" + i;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+               Console.WriteLine(arr[i].Autonummer); 
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i].Autonummer = "1";
+               Console.WriteLine(arr[i].Autonummer); 
+            } 
+        }
+    }
+}
