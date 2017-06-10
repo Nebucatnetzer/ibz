e2bdeaf2ca839a4d1413f76b3eb12b91196413f0
Andreas Zweili <andreas@capricorn>
2017-06-10 09:06:28 +0200

    create project about arrays

---
+++ software/1_sem/arrays/arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr[4] = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                 Console.WriteLine(arr[i]);               
+            } 
+            
+            Console.ReadLine();
+        }
+    }
+}
