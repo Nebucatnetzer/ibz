da1cc7d7f44a319b7849cea686e16761b62c4c70
Andreas Zweili <andreas@capricorn>
2017-07-05 15:17:43 +0200

    add the solution to the log entry excercise

---
+++ software/1_sem/se-12-log-entry/se-12-log-entry/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace se_12_log_entry
+{
+    public class LogEntry
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+
+        public LogEntry(int _id, DateTime _date)
+        {
+            this.ID = _id;
+            this.Date = _date;
+        }
+    }
+}
--- software/1_sem/se-12-log-entry/se-12-log-entry/Program.cs
+++ software/1_sem/se-12-log-entry/se-12-log-entry/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 
 namespace se_12_log_entry
 {
@@ -7,6 +8,20 @@ namespace se_12_log_entry
     {
         public static void Main(string[] args)
         {
+            Stack<LogEntry> logEntries = new Stack<LogEntry>();
+            int id_counter = 0;
+            while (id_counter < 11)
+            {
+                logEntries.Push(new LogEntry(id_counter, DateTime.Now));
+                    id_counter++;
+
+                LogEntry newestEntry = logEntries.Peek();
+                Console.WriteLine("ID:"
+                                  + newestEntry.ID
+                                  + " Date:"
+                                  + newestEntry.Date);
+                System.Threading.Thread.Sleep(2000);
+            }
         }
     }
 }
