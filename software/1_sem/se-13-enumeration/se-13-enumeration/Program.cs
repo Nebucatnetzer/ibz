d785e6afb322fc6dc8cd2ab71e83fa546c152158
Andreas Zweili <andreas@capricorn>
2017-07-08 09:58:12 +0200

    add an excercise about enumerations

---
+++ software/1_sem/se-13-enumeration/se-13-enumeration/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace se_13_enumeration
+{
+
+    public enum Colors
+    {
+       Red,
+       Green,
+       Blue
+    }
+    
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some text to colorize:");
+            string consoleInput = Console.ReadLine();
+            writeColoredText(Colors.Red, consoleInput);
+            Console.ReadKey();
+        }
+        
+        static void writeColoredText(Colors c, string s)
+        {
+            switch (c)
+            {
+                    case Colors.Blue:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Colors.Green:    
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Colors.Red:    
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                     
+            }
+        Console.WriteLine(s);
+    
+        }
+    }
+}
