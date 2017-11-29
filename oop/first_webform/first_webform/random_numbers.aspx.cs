21ba54ac0900989030e9fb52eb69374265d2d585
Andreas Zweili <andreas@2li.ch>
2017-11-29 21:25:02 +0100

    add asp.net excercise to generate random number

---
+++ oop/first_webform/first_webform/random_numbers.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace first_webform
+{
+    public partial class random_numbers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        if (!Page.IsPostBack)
+            {
+            int iterations = 10;
+            Random rnd = new Random();
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    var paragraph = new HtmlGenericControl("p");
+
+                    int random_number = rnd.Next(1, 101);
+                    paragraph.InnerHtml = Convert.ToString(random_number);
+                    form1.Controls.Add(paragraph);
+                }
+            }
+        }
+    }
+}
