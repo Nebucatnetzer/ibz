d4bcc90753e7b15baa052aa04040465ec0c4f5bf
Andreas Zweili <andreas@2li.ch>
2017-12-02 09:53:50 +0100

    add the multiplication excercise

---
+++ oop/first_webform/first_webform/multiplication_table.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="multiplication_table.aspx.cs" Inherits="first_webform.multiplication_table" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css" media="screen">
+        table{
+        border-collapse:collapse;
+        border:1px solid #000000;
+        }
+
+        table td{
+        border:1px solid #000000;
+        }
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Table ID="Table1" runat="server">
+        </asp:Table>
+    </form>
+</body>
+</html>
---
+++ oop/first_webform/first_webform/multiplication_table.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace first_webform
+{
+    public partial class multiplication_table : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        if (!Page.IsPostBack)
+            {
+                // Total number of rows.
+                int rowCnt;
+                // Current row count.
+                int rowCtr;
+                // Total number of cells per row (columns).
+                int cellCtr;
+                // Current cell counter
+                int cellCnt;
+
+                rowCnt = 10;
+                cellCnt = 10;
+
+                for (rowCtr = 1; rowCtr <= rowCnt; rowCtr++)
+                {
+                    // Create new row and add it to the table.
+                    TableRow tRow = new TableRow();
+                    Table1.Rows.Add(tRow);
+                    for (cellCtr = 1; cellCtr <= cellCnt; cellCtr++)
+                    {
+                        // Create a new cell and add it to the row.
+                        TableCell tCell = new TableCell();
+                        tCell.Text = Convert.ToString(rowCtr * cellCtr);
+                        tRow.Cells.Add(tCell);
+                    }
+                }
+
+            }
+        }
+    }
+}
