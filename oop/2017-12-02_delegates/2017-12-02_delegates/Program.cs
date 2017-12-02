e18253ce658abaa03c70680b35613d43a9647527
Andreas Zweili <andreas@2li.ch>
2017-12-02 10:44:15 +0100

    add solution for a delegates excercise

---
+++ oop/2017-12-02_delegates/2017-12-02_delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_12_02_delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delWrite dw;
+            dw = WriteGreen;
+            dw += WriteRed;
+            dw("Hello World");
+            Console.ReadKey();
+        }
+
+        public static void WriteGreen(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+        }
+
+        public static void WriteRed(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(s);
+        }
+
+        public delegate void delWrite(string s);
+
+        
+    }
+}
