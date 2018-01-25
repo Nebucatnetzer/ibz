b735923ebd443fabd3cbf14f81772876a29c87e0
Andreas Zweili <andreas@2li.ch>
2018-01-25 18:57:19 +0100

    add a missing file

---
+++ oop/notenbewertung/notenbewertung/Testresultat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notenbewertung
+{
+    class Testresultat
+    {
+        public double Note { get; set; }
+        public Testresultat(double d)
+        {
+            this.Note = d;
+        }
+
+    }
+}
