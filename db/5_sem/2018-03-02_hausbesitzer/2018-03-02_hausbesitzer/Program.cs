2bfff5aee1e84207dd111c3cec3ba8200ebea786
Andreas Zweili <andreas@2li.ch>
2018-03-02 19:00:32 +0100

    add a solution for the "hausbesitzer" case study

---
+++ db/5_sem/2018-03-02_hausbesitzer/2018-03-02_hausbesitzer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_03_02_hausbesitzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
