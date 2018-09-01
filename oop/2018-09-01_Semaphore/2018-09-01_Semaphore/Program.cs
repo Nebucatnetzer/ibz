1c14a2fb2d9c00e3d907e57117915dd09758b182
Andreas Zweili <andreas@2li.ch>
2018-09-01 12:01:44 +0200

    add excercises for threads and semaphores

---
+++ oop/2018-09-01_Semaphore/2018-09-01_Semaphore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _2018_09_01_Semaphore
+{
+    class Program
+    {
+        static Semaphore semaphoreObject = new Semaphore(3, 3);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(DoWork);
+                thread.Name = "Thread_" + i;
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+        static void DoWork()
+        {
+            Console.WriteLine("{0} is waiting in QUEUE...", Thread.CurrentThread.Name);
+            // Thread waits to get an available slot
+            semaphoreObject.WaitOne();
+            Console.WriteLine("{0} enters the Critical Section!", Thread.CurrentThread.Name);
+            // Simulate work by waiting for a few seconds
+            Thread.Sleep(5000);
+            Console.WriteLine("{0} is leaving the Critical Section", Thread.CurrentThread.Name);
+            // Release the slot.
+            semaphoreObject.Release();
+        }
+    }
+}
---
+++ oop/2018-09-01_threads/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2018_09_01_threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Method1);
+            Thread thread2 = new Thread(Method2);
+            thread1.Start();
+            thread2.Start();
+            Console.ReadLine();
+        }
+        static void Method1()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                lock (Locker)
+                {
+                    Console.WriteLine("T1 A " + Counter.ToString());
+                    Counter += 1;
+                    Console.WriteLine("T1 B " + Counter.ToString());
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Method2()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                lock (Locker)
+                {
+                    Console.WriteLine("T2 A " + Counter.ToString());
+                    Counter += 1;
+                    Console.WriteLine("T2 B " + Counter.ToString());
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static int Counter { get; set; }
+        static object Locker = new object();
+    }
+}
